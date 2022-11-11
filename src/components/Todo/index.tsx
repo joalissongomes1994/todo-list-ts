4efa964204b8351ec0f4c170b3ffd1fff95f92c8
Joalisson Gomes <joalissongomes.webdev@gmail.com>
2022-11-11 02:17:34 -0300

    feat: Add listing functionality

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,31 +1,45 @@
+import { useState } from "react";
 import { TodoEmpty } from "../TodoEmpty";
 import { TaskItem } from "../TaskItem";
-import { Button } from "../Button";
+import { TaskForm } from "../TaskForm";
+import { v4 as uuidv4 } from "uuid";
+import { ITask } from "../../types";
 
 import style from "./Todo.module.css";
 
-const tasks = [1, 2, 3, 4, 5];
-
 export function Todo() {
+  const [tasks, setTasks] = useState<ITask[]>([]);
+
+  function handleCreateTask(task: string) {
+    setTasks(state => [
+      ...state, 
+      {
+        id: uuidv4(),
+        isComplete: false,
+        title: task
+      }
+    ])
+  }
+
   return(
     <div className={style.container}>
-      <Button />
+      <TaskForm onHandleCreateTask={handleCreateTask} />
 
         <div className={style.headerList}>
           <strong className={style.created}>
             Tarefas criadas
-            <span>5</span>
+            <span>{tasks.length}</span>
           </strong>
         
           <strong className={style.done}>
             Concluidas
-            <span>2 de 5</span>
+            <span>0</span>
           </strong>
         </div>
 
       {tasks.length === 0 
         ? <TodoEmpty /> 
-        : tasks.map(task => <TaskItem key={task} />)
+        : tasks.map(task => <TaskItem key={task.id} taskData={task}/>)
       }
     </div>
   )
