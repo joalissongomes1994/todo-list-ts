11ed2505c5d9b8c50a01b4ec8a4f0bd0062ce3a1
Joalisson Gomes <joalissongomes.webdev@gmail.com>
2022-11-11 02:25:36 -0300

    feat: Add props on the component

--- src/components/TaskItem/index.tsx
+++ src/components/TaskItem/index.tsx
@@ -1,16 +1,27 @@
 import style from "./TaskItem.module.css";
 import {BsFillCheckCircleFill, BsCircle, BsTrash} from "react-icons/bs"
+import { ITask } from "../../types";
+interface ITaskProps {
+  taskData: ITask
+}
 
-export function TaskItem() {
+export function TaskItem({ taskData: { id, isComplete, title} }: ITaskProps) {
   return(
     <div className={style.contentItem}>
       <button className={style.buttonCheck}>
-        <BsFillCheckCircleFill size={20} className={style.isChecked}/>
-        {/* <BsCircle size={20} className={style.isNotChecked} /> */}
+        {isComplete 
+          ? <BsFillCheckCircleFill size={20} className={style.isChecked}/> 
+          : <BsCircle size={20} className={style.isNotChecked} />
+        }
       </button>
-      <p className={`${style.taskDescription} ${style.taskDescriptionDone}`}>Integer urna interdum massa libero auctor neque 
-      turpis turpis semper. Duis vel sed fames integer.
-      </p>
+      <div className={style.taskDescriptionArea}>
+        <p 
+          className={`${style.taskDescription} ${isComplete && style.taskDescriptionDone}`}
+        >
+          {title}
+        </p>
+      </div>
+      
       <button className={style.buttonTrash}>
         <BsTrash size={20}/>
       </button>   
